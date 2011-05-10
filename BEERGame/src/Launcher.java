0f3b1210dd0e0f84b6020c3248f0adb6048fce32
unknown <localmgr@.rose-hulman.edu>
2011-05-10 15:50:16 -0400

    removed images

--- BEERGame/src/Launcher.java
+++ BEERGame/src/Launcher.java
@@ -34,20 +34,20 @@ public class Launcher {
 	 * on creation of a Launcher object, the game is created and initialized
 	 */
 	public Launcher() {
-		this.language = "english";
-		this.image = new ImageIcon("main.jpg");
-		this.p = new Player("Tyler");
-		Item taserItem = new Item("taser", "this taser hurts bad guys",new 
-				ImageIcon("taser.jpg"), new Region(0,0,32,60));
-		Region taserRegion = new Region(56, 320, 25, 60, taserItem);
-		
-		this.v3 = new View("french image view",
-				new ImageIcon("mainfrench.jpg"));
-		this.v1 = new View("main image view", image, v3);
-		this.v2 = new View("taser is gone!",new ImageIcon("notaser.jpg"));
-		v1.addRegion(taserRegion);
-		v1.addView(v2);
-		this.game = new Game(p, v1);
+//		this.language = "english";
+//		this.image = new ImageIcon("images/main.jpg");
+//		this.p = new Player("Tyler");
+//		Item taserItem = new Item("taser", "this taser hurts bad guys",new 
+//				ImageIcon("images/taser.jpg"), new Region(0,0,32,60));
+//		Region taserRegion = new Region(56, 320, 25, 60, taserItem);
+//		
+//		this.v3 = new View("french image view",
+//				new ImageIcon("images/mainfrench.jpg"));
+//		this.v1 = new View("main image view", image, v3);
+//		this.v2 = new View("taser is gone!",new ImageIcon("images/notaser.jpg"));
+//		v1.addRegion(taserRegion);
+//		v1.addView(v2);
+//		this.game = new Game(p, v1);
 		
 
 	}
@@ -84,7 +84,7 @@ public class Launcher {
 	 */
 	public static void main(String[] args) throws Exception {
 		Launcher l = new Launcher();
-		//l.writeGame(l);
+	//	l.writeGame(l);
 		
 		
 		
